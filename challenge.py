33f5f5db0a23a1a5a1e42205e01296e2339e916f
DeltaF1 <delyaerricson@gmail.com>
2018-01-10 20:38:06 -0500

    Reorganized and added comments

--- challenge.py
+++ challenge.py
@@ -1,65 +1,83 @@
 import json
 import requests
 
-CHALLENGE_ID = 1
 
-url = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=" + CHALLENGE_ID + "&page="
+#Return the ids of all children of a given node
+def get_children(menus, menu, seen):
+	#Assume all children are valid
+	valid = True
+	
+	#If we're at an end node, stop recursing
+	if not menu["child_ids"]:
+		return [], True
+	
+	
+	ids = []
+	
+	for id in menu["child_ids"]:
+		if id in seen:
+			valid = False
+		else:
+			seen.append(id)
+			if id not in menus:
+				#invalid: missing menu
+				valid = False
+				continue
+			
+			#Add child to current list of ids before recursing
+			#Without this line, non-end nodes won't be added to the list
+			ids.append(id)
+			
+			ret_ids, ret_valid = get_children(menus, menus[id], seen)
+			
+			#Add child ids to current tally
+			ids += ret_ids
+			
+			#This node is only valid if all subnodes are also valid
+			valid = valid and ret_valid
+			
+	return ids, valid
 
-menus = {}
+def main(url):
+	menus = {}
+	page = 0
 
-page = 0
+	while True:
+		page += 1
+		r = requests.get(url+str(page))
+		response = json.loads(r.text)
 
-while True:
-    r = requests.get(url+str(page))
-    response = json.loads(r.text)
+		#Add menu objects to a dictionary keyed by id
+		for menu in response["menus"]:
+			menus[menu["id"]] = menu
 
-    for menu in response['menus']:
-        menus[menu['id']] = menu
+		if len(menus) >= response["pagination"]["total"]:
+			break
+		
+	#print(json.dumps(menus, indent=4, sort_keys=True))
 
-    page += 1
-    print("nodes: "+str(len(menus)))
+	#Isolate root menus
+	parents = [menus[id] for id in menus if "parent_id" not in menus[id]]   
 
-    if len(menus) >= response['pagination']['total']:
-        break
-    
-print(json.dumps(menus, indent=4, sort_keys=True))
+	valid = []
+	invalid = []
 
-parents = [menus[id] for id in menus if "parent_id" not in menus[id]]   
+	for parent in parents:
+		
+		child_ids, valid_flag = get_children(menus, parent, [])
 
-valid = []
-invalid = []
+		return_value = {"root_id" : parent['id'], "children" : child_ids}
 
-def get_children(menu, seen):
-    valid = True
-    if not menu["child_ids"]:
-        return [], True
-    ids = []
-    for id in menu["child_ids"]:
-        if id in seen:
-            valid = False
-        else:
-            seen.append(id)
-            if id not in menus:
-                #invalid: missing menu
-                valid = False
-                continue
-            ids.append(id)
-            child = menus[id]
-            ret_ids, ret_valid = get_children(child, seen)
-            ids += ret_ids
-            valid = valid and ret_valid
-    return ids, valid
+		if valid_flag:
+			valid.append(return_value)
+		else:
+			invalid.append(return_value)
+			
+	print(json.dumps({"valid_menus" : valid, "invalid_meus" : invalid}, indent=4))
 
-for parent in parents:
-    
-    
-    child_ids, valid_flag = get_children(parent, [])
+if __name__ == '__main__':
+	CHALLENGE_ID = input("Which challenge? (1|2) ")
 
-    return_value = {"root_id" : parent['id'], "children" : child_ids}
+	url = "https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=" + CHALLENGE_ID + "&page="
 
-
-    if valid_flag:
-        valid.append(return_value)
-    else:
-        invalid.append(return_value)
-        
+	main(url)
